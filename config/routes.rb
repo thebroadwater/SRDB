19d910d61f02719cc3336d82aea15b3a4639a6c6
Michael Broadwater <mike.broadwater@gmail.com>
2017-04-26 21:14:25 -0400

    don't let new users register

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # devise_for :users, :skip => [:registrations]
+  devise_for :users, :skip => [:registrations]
   devise_for :users
 
   # authenticate :user do
