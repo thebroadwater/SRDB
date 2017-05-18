d02eb416a96beb37375272707329dfdfe4d11a3f
Michael Broadwater <mike.broadwater@gmail.com>
2017-05-17 22:28:16 -0400

    destroy dependents

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
-  has_many :group_sources
-  has_many :occurrence_groups
+  has_many :group_sources, dependent: :destroy
+  has_many :occurrence_groups, dependent: :destroy
 
   searchkick highlight: [:name, :summary, :details, :kind, :subtype, :notes]
     # word_start: [:name, :summary, :details, :kind, :subtype, :notes]
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
 class Location < ApplicationRecord
-  has_many :location_sources
-  has_many :occurrence_locations
+  has_many :location_sources, dependent: :destroy
+  has_many :occurrence_locations, dependent: :destroy
 
   searchkick highlight: [:name, :summary, :details, :kind, :address, :city, :country]
     # word_start: [:name, :summary, :description, :kind, :address, :city, :country]
