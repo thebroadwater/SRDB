cfbf593595e0334299f7c5a3b0bbb08c6aa01939
Michael Broadwater <17167283+thebroadwater@users.noreply.github.com>
2023-07-28 12:54:41 -0400

    disallow POST

--- api/views.py
+++ api/views.py
@@ -1,17 +1,21 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.response import Response
 
 from api.serializers import ProductSerializer, EventSerializer
 from api.models import Products, Editions, Publishers, Events
 
 
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductViewSet(mixins.RetrieveModelMixin, 
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
     queryset = Products.objects.all().order_by('sku')
     serializer_class = ProductSerializer
     filterset_fields = ['name', 'sku', 'id', 'own']    
 
 
-class EventViewSet(viewsets.ModelViewSet):
+class EventViewSet(mixins.RetrieveModelMixin, 
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
     queryset = Events.objects.all().order_by('date')
     serializer_class = EventSerializer
  
